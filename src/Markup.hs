2e7188cd30d8539226be654a5c53f121b18d86f4
Fraser Murray <fraser.m.murray@gmail.com>
2015-04-09 18:44:30 +0100

    clean up html

--- src/Markup.hs
+++ src/Markup.hs
@@ -7,7 +7,8 @@ homepage :: Html ()
 homepage = withHeader $
   form_ [method_ "POST", action_ "/upload", enctype_ "multipart/form-data"] $ do
     input_ [type_ "file", name_ "upload", accept_ "image/png"]
-    input_ [type_ "submit"]
+    br_ []
+    input_ [type_ "submit", value_ "compress"]
 
 uploadNotFound :: Html ()
 uploadNotFound = withHeader "uploaded file not found"
@@ -15,11 +16,12 @@ uploadNotFound = withHeader "uploaded file not found"
 withHeader :: Html () -> Html ()
 withHeader x = do
   doctype_
-  head_ $
+  head_ $ do
     title_ "crusher"
-  body_ $ do
-    a_ [href_ "/"] $ h1_ "crusher"
-    x
+  body_ $
+    div_ [id_ "main"] $ do
+      a_ [href_ "/"] $ h1_ "crusher"
+      x
 
 error :: String -> Html ()
 error err = withHeader $ do
